a31e7f1faa8b725c21ce2fd8441f56980644a689
Riccardo Righi <rr9379@gmail.com>
2017-10-18 11:32:16 +0200

    Initial setup and configuration

---
+++ src/main/java/com/holonplatform/example/api/ApiEndpoint.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2016-2017 Axioma srl.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.holonplatform.example.api;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.springframework.stereotype.Component;
+
+import com.holonplatform.jaxrs.swagger.annotations.ApiDefinition;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+/**
+ * JAX-RS API endpoint.
+ */
+@ApiDefinition(docsPath = "/api/docs", title = "Example API", version = "v1", prettyPrint = true)
+@Api
+@Component
+@Path("/api")
+public class ApiEndpoint {
+
+	@ApiOperation("Ping request")
+	@ApiResponses({ @ApiResponse(code = 200, message = "OK: pong", response = String.class) })
+	@GET
+	@Path("/ping")
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response ping() {
+		return Response.ok("pong").build();
+	}
+
+}
---
+++ src/test/java/com/holonplatform/example/test/ApiTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2016-2017 Axioma srl.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.holonplatform.example.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.holonplatform.http.HttpHeaders;
+import com.holonplatform.http.HttpStatus;
+import com.holonplatform.http.rest.ResponseEntity;
+import com.holonplatform.http.rest.RestClient;
+
+/**
+ * API unit test
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+public class ApiTest {
+
+	@Test
+	public void testPing() {
+
+		ResponseEntity<String> response = RestClient.forTarget("http://localhost:9999/api").request().path("ping")
+				.get(String.class);
+
+		Assert.assertEquals(HttpStatus.OK, response.getStatus());
+		Assert.assertEquals("pong", response.as(String.class).orElse(null));
+	}
+
+	@Test
+	public void testApiDocs() {
+
+		// as JSON
+		ResponseEntity<String> response = RestClient.forTarget("http://localhost:9999/api/docs").request()
+				.get(String.class);
+		Assert.assertEquals(HttpStatus.OK, response.getStatus());
+		Assert.assertTrue(response.getPayload().isPresent());
+		Assert.assertEquals("application/json", response.getHeaderValue(HttpHeaders.CONTENT_TYPE).orElse(null));
+
+		// as YAML
+		response = RestClient.forTarget("http://localhost:9999/api/docs").request().queryParameter("type", "yaml")
+				.get(String.class);
+		Assert.assertEquals(HttpStatus.OK, response.getStatus());
+		Assert.assertTrue(response.getPayload().isPresent());
+		Assert.assertEquals("application/yaml", response.getHeaderValue(HttpHeaders.CONTENT_TYPE).orElse(null));
+	}
+
+	@Test
+	public void testActuator() {
+		ResponseEntity<String> response = RestClient.forTarget("http://localhost:9999/info").request()
+				.get(String.class);
+		Assert.assertEquals(HttpStatus.OK, response.getStatus());
+	}
+
+}
